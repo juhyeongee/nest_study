ce139265d9ef87bd61e6d1f9ed5d597fd0faaa52
juhyeong kim <juhyeongee@gmail.com>
2023-10-30 16:37:11 +0900

    service & controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,26 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-//nest g resouce를 통해 만듦
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @Get('post')
-  getPost(): Post {
-    return {
-      author: "newjeans officail",
-      title: "민지",
-      content: "ㅇ예아",
-      likeCount: 9999,
-      commentCount: 100000
-    }
+  // 1) GET / posts 
+  // 모든 post를 다 가져온다.
+  
+  @Get()
+  getPosts(){
+    return this.postsService.getAllPosts();
+  }
+
+  // 2) GET / posts/:id
+  // id에 해당되는 post를 가져온다 
+  // 예를들어, id=1 인경우 id가 1인 포스트를 가져온다. 
+  // Get(:id)를 통해 간단하게 패스파라미터를 설정할 수 있다.
+  // @Param('id') id:string 에서 'id'(파라미터 이름)를 통해서 어떤 파라미터를 가져올지 명시함
+  // /posts/:id/:name/:age 등 여러개의 파라미터를 쓸 수 있기 때문에 그런 경우에 유용함
+  @Get(':id')
+  getPost(@Param('id') id:string ){
+    return this.postsService.getPostById(+id);
+  }
+  //
+  // 3) POST / posts
+  //  POST를 생성한다
+  @Post()
+  postPosts(
+    @Body('author') author:string, 
+    @Body('title') title: string,
+    @Body('content') content: string, 
+  ){
+      return this.postsService.createPost(author, title, content);
+  }
+  // 4) PATCH / posts
+  //   POST를 변경한다 
+
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?:string, 
+    @Body('title') title?: string,
+    @Body('content') content?: string, 
+  ){
+     return this.postsService.updatePost(id,author, content, title);
+  }
+
+  // 5) DELETE /posts/:id 
+  //    id에 해당되는 POST를 삭제한다. 
+
+  @Delete(':id')
+  deletePost(@Param('id') id:string){
+    return this.postsService.deletePost(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+export interface PostModel {
+    id: number;
+    author: string;
+    title: string;
+    content: string;
+    likeCount: number;
+    commentCount: number;
+  }
+  //nest g resouce를 통해 만듦
+  
+  let posts: PostModel[] = [
+    {
+      id:1 ,
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content : '메이크업 고치는 민지',
+      likeCount : 100000,
+      commentCount : 99999,
+    },
+    {
+      id: 2 ,
+      author: 'newjeans_official',
+      title: '뉴진스 해린',
+      content : '노래연습하는 해린',
+      likeCount : 100000,
+      commentCount : 99999,
+    },
+    {
+      id: 3 ,
+      author: 'blackpink_official',
+      title: '로제',
+      content : '공연하는 로제',
+      likeCount : 100000,
+      commentCount : 99999,
+    }
+  
+  ]
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+    getAllPosts(){
+        return posts;
+    }
+
+    getPostById(id:number){
+        const post =  posts.find((post)=> post.id === +id);
+    if(!post){
+      throw new NotFoundException();
+    }
+    //자주쓰는 Exception
+    // 1. BadRequestException
+    // 2. UnauthorizedException
+    // 3. NotFoundException
+    // 4. ForbiddenException
+
+    return post;
+    }
+
+
+    createPost(author: string, title: string, content: string){
+        const post : PostModel = {
+            id: posts[posts.length -1].id + 1, 
+            author : author, 
+            title ,
+            content,
+            // key와 value가 같으면 생략해줄 수 있는 문법
+            likeCount : 0 ,
+            commentCount : 0 , 
+          }
+      
+          posts = [
+            ...posts,
+            post
+          ]; 
+      
+          return post;
+    }
+
+    updatePost(id: string, author: string, title: string, content: string){
+        const post = posts.find(post => post.id === +id);
+        if(!post){
+          throw new NotFoundException();
+        }
+  
+        if(author){
+          post.author = author; 
+        }
+  
+        if(title){
+          post.title = title;
+        }
+        
+        if(content){
+          post.content = content;
+        }
+  
+        posts = posts.map(prevPost => prevPost.id === +id ? post : prevPost);
+  
+        return post;
+    }
+
+    deletePost(id:string )  {
+        const post =  posts.find((post)=> post.id === +id);
+        if(!post){
+          throw new NotFoundException();
+        }
+        posts = posts.filter(post => post.id !== +id);
+        return id; 
+    }
+}
